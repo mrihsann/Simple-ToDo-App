f15f689f8d09eda2297acf386d5b4a0d3eaa4d7a
mrihsann <thearslanihsan@gmail.com>
2025-04-21 16:27:10 +0300

    Create AppModule to provide TodoDatabase
    The `AppModule` is created as a Hilt module, providing a `TodoDatabase` instance using Room. It uses `@Module`, `@InstallIn(SingletonComponent::class)`, `@Provides`, and `@Singleton` annotations for dependency injection. The database is built with the name "tododb" and uses `ApplicationContext`.

--- app/src/main/java/com/ihsanarslan/simpletodoapp/di/AppModule.kt
+++ app/src/main/java/com/ihsanarslan/simpletodoapp/di/AppModule.kt
@@ -1,11 +1,27 @@
 package com.ihsanarslan.simpletodoapp.di
 
+import android.content.Context
+import androidx.room.Room
+import com.ihsanarslan.simpletodoapp.data.local.TodoDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): TodoDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            TodoDatabase::class.java,
+            "tododb"
+        ).build()
+    }
 
 }
