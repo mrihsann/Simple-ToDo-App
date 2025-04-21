05553b21de1cbd341dfeb662c7c5c356a60d4eb5
mrihsann <thearslanihsan@gmail.com>
2025-04-21 16:15:25 +0300

    Apply AndroidEntryPoint to MainActivity
    The `@AndroidEntryPoint` annotation is added to the `MainActivity` class to enable Hilt dependency injection.

--- app/src/main/java/com/ihsanarslan/simpletodoapp/ui/MainActivity.kt
+++ app/src/main/java/com/ihsanarslan/simpletodoapp/ui/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import com.ihsanarslan.simpletodoapp.ui.theme.MyappTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
