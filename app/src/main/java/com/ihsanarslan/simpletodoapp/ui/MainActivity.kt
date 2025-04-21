a05af3a9204cc68cd8b09b10712635d5447424bf
mrihsann <thearslanihsan@gmail.com>
2025-04-21 16:15:05 +0300

    Initial commit

---
+++ app/src/main/java/com/ihsanarslan/simpletodoapp/ui/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ihsanarslan.simpletodoapp.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.ihsanarslan.simpletodoapp.ui.theme.MyappTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyappTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Text(text = "Hello, World!", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
