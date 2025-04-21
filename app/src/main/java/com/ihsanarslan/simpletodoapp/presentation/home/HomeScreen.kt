2025aa4d7ff900647b2c9ca7c2280f4228abaaff
mrihsann <thearslanihsan@gmail.com>
2025-04-21 17:09:46 +0300

    Add a floating action button to HomeScreen
    A floating action button with an add icon is added to the `HomeScreen` layout.

--- app/src/main/java/com/ihsanarslan/simpletodoapp/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/ihsanarslan/simpletodoapp/presentation/home/HomeScreen.kt
@@ -5,9 +5,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -30,6 +34,13 @@ fun HomeScreen(){
                     Text("Home Screen")
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+
+            }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
         }
     ) { paddings ->
         LazyColumn (
