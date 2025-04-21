6fbdd99a382e8f253717293336aa56d28bd9506e
mrihsann <thearslanihsan@gmail.com>
2025-04-21 16:46:57 +0300

    Create HomeScreen composable
    A new `HomeScreen` composable is created using Jetpack Compose, utilizing `Scaffold` for the layout. It displays a list of to-do items fetched from a `HomeScreenViewModel` and rendered with `LazyColumn`, `Card`, and `Text`. The screen also features a `CenterAlignedTopAppBar` for the title. `hiltViewModel` is used to inject the `HomeScreenViewModel`.

---
+++ app/src/main/java/com/ihsanarslan/simpletodoapp/presentation/home/HomeScreen.kt
@@ -0,0 +1,49 @@
+package com.ihsanarslan.simpletodoapp.presentation.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(){
+
+    val viewModel = hiltViewModel<HomeScreenViewModel>()
+    val todoList = viewModel.list.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text("Home Screen")
+                }
+            )
+        }
+    ) { paddings ->
+        LazyColumn (
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = paddings
+        ){
+            items(todoList.value.size) {
+                Card {
+                    Column {
+                        Text(text = todoList.value[it].title)
+                        Text(text = todoList.value[it].description)
+                    }
+                }
+            }
+        }
+    }
+}
