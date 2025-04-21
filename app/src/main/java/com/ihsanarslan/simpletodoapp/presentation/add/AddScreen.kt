05b0fb250dcda272ca3c96ac58cbb656fbf93ace
mrihsann <thearslanihsan@gmail.com>
2025-04-21 17:24:20 +0300

    Create AddScreen for adding to-do items
    A new `AddScreen` composable function is created. It allows users to input a title and description for a new to-do item. When the "Add" button is clicked, it creates a `TodoEntity` and inserts it using `AddScreenViewModel`.

---
+++ app/src/main/java/com/ihsanarslan/simpletodoapp/presentation/add/AddScreen.kt
@@ -0,0 +1,54 @@
+package com.ihsanarslan.simpletodoapp.presentation.add
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.ihsanarslan.simpletodoapp.data.local.TodoEntity
+
+@Composable
+fun AddScreen(){
+
+    val viewModel = hiltViewModel<AddScreenViewModel>()
+
+    val title = remember { mutableStateOf("") }
+    val description = remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        TextField(
+            value = title.value,
+            onValueChange = {
+                title.value = it
+            }
+        )
+        Spacer(Modifier.height(30.dp))
+        TextField(
+            value = description.value,
+            onValueChange = {
+                description.value = it
+            }
+        )
+        Spacer(Modifier.height(30.dp))
+        Button(
+            onClick = {
+                val todo = TodoEntity(title = title.value, description = description.value)
+                viewModel.insertTodo(todo)
+            }
+        ) {
+            Text("Add")
+        }
+    }
+}
